16f4df4cd697dfcb74bd1a2fbd40ad193e10a133
HarryLynn-afk <kyawnainglin1130@gmail.com>
2025-02-21 20:48:19 +0700

    Add files via upload

---
+++ the_band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+hometown_name = input("Enter your hometown's name\n")
+pet_name = input("Enter your pet's name\n")
+
+print("Your band name will be: " + hometown_name + " " + pet_name)
+
+
